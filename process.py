10e9fbe8a5fa1b4db6f53e122744f549a3b465bd
Peter M. Landwehr <plandweh@andrew.cmu.edu>
2018-04-04 22:26:12 -0700

    Reformat main

--- process.py
+++ process.py
@@ -1,9 +1,12 @@
-import pandas as pd
 import glob
-import numpy as np
-from matplotlib import pylab as plt
-from tqdm import trange
 import os
+from collections import defaultdict
+from tqdm import trange
+
+import numpy as np
+import numpy.random as npr
+import pandas as pd
+from matplotlib import pylab as plt
 
 from sklearn.metrics import roc_curve, roc_auc_score, accuracy_score
 from sklearn.linear_model import LogisticRegression
@@ -87,30 +90,28 @@ def shuffle_train(X, Y, teX):
     """Fit a model on shuffled samples and labels X and Y, and return prediction on the training data and on test samples TeX"""    
     pipe = make_pipeline(StandardScaler(), LogisticRegression(C=0.7))
 
-    idx = np.random.permutation(np.arange(len(X)))
+    idx = npr.permutation(np.arange(len(X)))
     pipe.fit(X[idx], Y[idx])
     
     return pipe.predict_proba(X)[:, 1], pipe.predict_proba(teX)[:, 1]
 
 def main():
     name_to_labels = load_labels()
-    for k,v in name_to_labels.items():
-        name_to_labels[k] = [0 if i==0 else 1 for i in v]
-    name_to_feats = {}
-    for k,v in name_to_labels.items():
-        name_to_feats[k] = []
-        for i,_ in enumerate(v):
-            # draws 1 index
-            frame = load_raw(k, i+1)
-            if frame is not None:
-                feat = features(frame, draw_num=i)
+    for name in name_to_labels:
+        name_to_labels[name] = [0 if i == 0 else 1 for i in name_to_labels[name]]
+    
+    name_to_feats = defaultdict(list)
+    for name in name_to_labels:
+        for i in range(len(name_to_labels[name])):
+            frame = load_raw(name, i+1)
+            if frame is None:
+                name_to_feats[name].append(None)
             else:
-                feat = None
-            name_to_feats[k].append(feat)
+                name_to_feats[name].append(features(frame, draw_num=i))
 
-    #max_feats = max([len(x) for x in name_to_feats.values()[0] if x is not None])
-    #for k,v in name_to_feats.items():
-        #for i in range(len(v)):
+    # max_feats = max([len(x) for x in name_to_feats.values()[0] if x is not None])
+    # for k,v in name_to_feats.items():
+        # for i in range(len(v)):
             #v[i] = v[i] + [0]*(max_feats - len(v[i]))
 
 
@@ -120,34 +121,34 @@ def main():
     for x in trange(10000):
         patients = name_to_labels.keys()
         n_train = int(len(patients) * .8)
-        train_pat = np.random.choice(patients, size=n_train).tolist()
+        train_pat = npr.choice(patients, size=n_train).tolist()
         test_pat = list(set(patients) - set(train_pat))
 
-        trX, trY = gather_feats_labels(train_pat, name_to_feats, name_to_labels)
-        teX, teY = gather_feats_labels(test_pat, name_to_feats, name_to_labels)
-        if len(np.unique(teY)) == 1:
+        train_X, train_Y = gather_feats_labels(train_pat, name_to_feats, name_to_labels)
+        test_X, test_Y = gather_feats_labels(test_pat, name_to_feats, name_to_labels)
+        
+        if len(np.unique(test_Y)) == 1:
             continue
-        if len(np.unique(trY)) == 1:
+        if len(np.unique(train_Y)) == 1:
             continue
+        
         X, Y = gather_feats_labels(patients, name_to_feats, name_to_labels)
-        idx = np.arange(len(X))
-        np.random.shuffle(idx)
-        spt = int(len(X)*.8)
-        trX = X[idx[:spt]]
-        teX = X[idx[spt:]]
-        trY = Y[idx[:spt]]
-        teY = Y[idx[spt:]]
+        idx = npr.permutation(np.arange(len(X)))
+        partition = int(len(X)*.8)
+        train_X = X[idx[:partition]]
+        test_X = X[idx[partition:]]
+        train_Y = Y[idx[:partition]]
+        test_Y = Y[idx[partition:]]
 
+        train_Y_pred, test_Y_pred = shuffle_train(train_X, train_Y, test_X)
 
-        trPY, tePY = shuffle_train(trX, trY, teX)
+        heldout_auc = roc_auc_score(test_Y, test_Y_pred)
+        train_auc = roc_auc_score(train_Y, train_Y_pred)
 
-        heldout_auc = roc_auc_score(teY, tePY)
-        train_auc = roc_auc_score(trY, trPY)
-
-        fpr, tpr, _ = roc_curve(teY, tePY)
+        fpr, tpr, _ = roc_curve(test_Y, test_Y_pred)
         accum.append([heldout_auc, train_auc])
     
-    print np.mean(accum, axis=0), np.var(accum, axis=0)
+    print('Mean: {}, Variance: {}'.format(np.mean(accum, axis=0), np.var(accum, axis=0)))
     accum = np.asarray(accum)
     plt.subplot(2,1,1)
     plt.title("Histogram of AUC from ML based model")
