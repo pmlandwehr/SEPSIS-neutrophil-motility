1f6d2e5a12cf0049121ff85dd6dd3c012674f3f5
Peter M. Landwehr <plandweh@andrew.cmu.edu>
2018-04-05 09:30:09 -0700

    Update convert_burns.py

--- convert_burns.py
+++ convert_burns.py
@@ -1,8 +1,14 @@
-import glob
-for f in glob.glob("BURNS/*Patient*.xls"):
-    if "Patient" in f:
-        target = "BURNS_CSV/"+f.split("/")[-1] + "_%s.csv"
-        f = f.replace(" ", "\\ ")
-        target = target.replace(" ", "\\ ")
-        cmd = "ssconvert %s %s -S"%(f, target)
-        print cmd
+from glob import glob
+
+def main():
+    for f in glob("BURNS/*Patient*.xls"):
+        if "Patient" in f:
+            target = "BURNS_CSV/"+f.split("/")[-1] + "_%s.csv"
+            f = f.replace(" ", "\\ ")
+            target = target.replace(" ", "\\ ")
+            cmd = "ssconvert %s %s -S"%(f, target)
+            print cmd
+
+if __name__ == "__main__":
+    main()
+    
