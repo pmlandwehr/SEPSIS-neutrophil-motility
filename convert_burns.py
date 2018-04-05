87bfc20c1a19eb62edf56a911708f15d01061272
Peter M. Landwehr <plandweh@andrew.cmu.edu>
2018-04-05 09:34:51 -0700

    Un-nest loop

--- convert_burns.py
+++ convert_burns.py
@@ -2,12 +2,14 @@ from glob import glob
 
 def main():
     for f in glob("BURNS/*Patient*.xls"):
-        if "Patient" in f:
-            target = "BURNS_CSV/"+f.split("/")[-1] + "_%s.csv"
-            f = f.replace(" ", "\\ ")
-            target = target.replace(" ", "\\ ")
-            cmd = "ssconvert %s %s -S"%(f, target)
-            print cmd
+        if 'Patient' not in f:
+            continue
+        
+        target = "BURNS_CSV/"+f.split("/")[-1] + "_%s.csv"
+        f = f.replace(" ", "\\ ")
+        target = target.replace(" ", "\\ ")
+        cmd = "ssconvert %s %s -S"%(f, target)
+        print cmd
 
 if __name__ == "__main__":
     main()
