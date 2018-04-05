27bc657b3c6d92ea063b4b0ed10fbde1d556f6a8
Peter M. Landwehr <plandweh@andrew.cmu.edu>
2018-04-04 22:29:06 -0700

    Reformat category10

--- load_data.py
+++ load_data.py
@@ -7,17 +7,17 @@ from tqdm import trange
 files = glob.glob("csvs/*.csv")
 
 category10=[
-      "1f77b4"
-    , "ff7f0e"
-    , "2ca02c"
-    , "d62728"
-    , "9467bd"
-    , "8c564b"
-    , "e377c2"
-    , "7f7f7f"
-    , "bcbd22"
-    , "17becf"
-]
+      "1f77b4",
+      "ff7f0e",
+      "2ca02c",
+      "d62728",
+      "9467bd",
+      "8c564b",
+      "e377c2",
+      "7f7f7f",
+      "bcbd22",
+      "17becf"]
+
 def hex_to_rgb(value):
     lv = len(value)
     return tuple(
