2d4abe10aab71cd730a5c09c762d127c212dbd2b
Peter M. Landwehr <plandweh@andrew.cmu.edu>
2018-04-04 23:08:11 -0700

    Reformat with main, path separators

--- convert_data.py
+++ convert_data.py
@@ -1,11 +1,16 @@
-import glob
-for f in glob.glob("TRACKS4/Spots*.xls"):
-    target = "csvs/"+f.split("/")[-1] + ".csv"
-    f = f.replace(" ", "\\ ")
-    if "(" in f:
-        continue
-    #f = f.replace("(", "\(")
-    #f = f.replace(")", "\)")
-    target = target.replace(" ", "\\ ")
-    cmd = "ssconvert %s %s"%(f, target)
-    print cmd
+from glob import glob
+import os
+
+def main():
+    for f in glob(os.path.join('TRACKS4', 'Spots*.xls')):
+        target = os.path.join('csvs', '{}.csv'.format(f.split(os.path.sep)[-1]))
+        f = f.replace(' ', '\\ ')
+        if '(' in f:
+            continue
+        target = target.replace(' ', '\\ ')
+        cmd = 'ssconvert {} {}'.format(f, target)
+        print(cmd)
+   
+if __name__ == "__main__":
+    main()
+    
